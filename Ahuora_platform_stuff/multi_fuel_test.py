67d81f54daedb7b939e65a2cfcf92fa5e1b258cd
david <tianchengdavid@gmail.com>
2025-09-30 10:19:28 +1300

    removed CO from multi_test

--- Ahuora_platform_stuff/multi_fuel_test.py
+++ Ahuora_platform_stuff/multi_fuel_test.py
@@ -32,7 +32,7 @@ from idaes.core import FlowsheetBlock
 import idaes.logger as idaeslog
 from idaes.models.properties.modular_properties import GenericParameterBlock
 from  biomass_comb_pp import configuration 
-from  biomass_combustion_rp import BMCombReactionParameterBlock
+# from  biomass_combustion_rp import BMCombReactionParameterBlock
 
 #helmholtz import for water
 from idaes.models.properties.general_helmholtz import (
@@ -101,7 +101,6 @@ m.fs.R101.inlet.mole_frac_comp[0,"N2"].fix(0.18)
 m.fs.R101.inlet.mole_frac_comp[0,"O2"].fix(0.8)
 m.fs.R101.inlet.mole_frac_comp[0,"CO2"].fix(1e-20)
 m.fs.R101.inlet.mole_frac_comp[0,"H2O"].fix(1e-20) 
-m.fs.R101.inlet.mole_frac_comp[0,"CO"].fix(1e-20) 
 m.fs.R101.inlet.mole_frac_comp[0,"biomass"].fix(0.01) 
 m.fs.R101.inlet.mole_frac_comp[0,"uncombustible"].fix(1e-20)
 m.fs.R101.inlet.mole_frac_comp[0,"CH4"].fix(0.01)
@@ -115,4 +114,4 @@ m.fs.R101.initialize(outlvl=idaeslog.INFO)
 solver=SolverFactory("ipopt")
 status=solver.solve(m,tee=True)
 m.fs.R101.report()
-m.fs.R101.display()
+# m.fs.R101.display()
